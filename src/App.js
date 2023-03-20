94a7b0f3c353b7cdc3c55d7cefdecb3a85bf54e5
Daniel Trandafir <danieltrandafir@live.co.uk>
2023-03-20 19:05:42 +0000

    add Nav and see Map api

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import { Grid } from '@mui/material';
 
-import Header from "./components/Header"
-import List from "./components/List"
-import Map from "./components/Map"
+import {Header} from "./components/Header"
+import {List} from "./components/List"
+import {Map} from "./components/Map"
 
 export const App = () => {
     return (
         <>
             <Header />
-            <Grid>
-                <Grid>
+            <Grid container spacing={3} style={{width: '100%'}}>
+                <Grid item xs={12} md={4}>
                     <List />
                 </Grid>
-                <Grid>
+                <Grid item xs={12} md={8}>
                     <Map />
                 </Grid>
             
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,32 @@
+import GoogleMapReact from "google-map-react";
 
-export const Map
- = () => {
+import {Paper, Typography, useMediaQuery} from "@mui/material"
+import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
+import Rating from "@mui/material"
+
+
+
+export const Map = () => {
+
+    const isMobile = useMediaQuery('(min-width:600px)');
+
+    const coordinates = {lat: 0, lng: 0};
+
     return (
-        <h1>Map
+        <div style={{backgroundColor:"red", height:"100vh", width:"100vw"}}>
+            <GoogleMapReact 
+                bootstrapURLKeys={{key:'AIzaSyBZedU85aS5ychcuCD7dmRTpN1h8Vb68kc'}}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50, 50, 50, 50]}
+                options={''}
+                onChange={''}
+                onChildClick={''}
+            >
             
-        </h1>
+            </GoogleMapReact>
+          
+        </div>
     );
 }
